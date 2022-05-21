dcfdb9ca8254f73d1e93f63877d38e73d4c7e338
Isaac <isaac@rubenfamily.com>
2022-05-20 19:02:29 -0700

    stylin

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,12 @@
+import styles from "./Card.module.scss";
+
 const Card = () => (
-  <div>
-    <img src="https://raw.githubusercontent.com/isaec/isaec/main/github-metrics.svg" />
+  <div className={styles.Card}>
+    <p>isaac r</p>
+    <img
+      className={styles.metric}
+      src="https://raw.githubusercontent.com/isaec/isaec/main/github-metrics.svg"
+    />
   </div>
 );
 export default Card;
