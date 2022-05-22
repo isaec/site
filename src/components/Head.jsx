a2db44ac9f07909632663549c291cad051d61753
Isaac <isaac@rubenfamily.com>
2022-05-21 23:36:04 -0700

    lead head have additional style

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,4 +1,14 @@
 import styles from "./Head.module.scss";
 
-const Head = (props) => <p className={styles.Head}>{props.text}</p>;
+const Head = (props) => (
+  <p
+    classList={{
+      [styles.Head]: true,
+      // this allows funky indirection but its fine
+      [props.className ?? props.class ?? undefined]: true,
+    }}
+  >
+    {props.text}
+  </p>
+);
 export default Head;
--- src/components/Metric.jsx
+++ src/components/Metric.jsx
@@ -4,7 +4,7 @@ import Head from "./Head.jsx";
 
 const Metric = () => (
   <Card>
-    <Head text="isaac r; me." />
+    <Head className={styles.Head} text="isaac r; me." />
     <img
       className={styles.Metric}
       src="https://raw.githubusercontent.com/isaec/isaec/main/github-metrics.svg"
