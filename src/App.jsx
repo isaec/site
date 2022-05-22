0609bd37238c10b0450757bed4f5592b2a8532f2
Isaac <isaac@rubenfamily.com>
2022-05-21 23:45:54 -0700

    test multiple cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import styles from "./App.module.scss";
+import Card from "./components/Card";
+import Head from "./components/Head";
 import Metric from "./components/Metric";
 
 const App = () => {
   return (
     <div className={styles.App}>
       <Metric />
+      <Card>
+        <Head text="who?" />
+        <p>test of the nature of god</p>
+      </Card>
+      <Card>
+        <Head text="third card !" />
+        <p>what is the meaning of this?</p>
+        <Head text="why ??" />
+        <p>makes sense to me...</p>
+      </Card>
     </div>
   );
 };
