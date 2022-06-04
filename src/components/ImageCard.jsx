728182b6790daaeaadc692eb5911fec794c5a8eb
Isaac <isaac@rubenfamily.com>
2022-06-04 16:44:30 -0700

    padding around image

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 import styles from "./ImageCard.module.scss";
 
 const ImageCard = (props) => (
-  <Card className={styles.ImageCard}>
+  <Card className={styles.ImageCard} pad={false}>
     <img
       src={props.src}
       alt={props.alt}
