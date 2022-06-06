236f9c06d7db53ef4dc8cf6a3e272530d461d991
Isaac <isaac@rubenfamily.com>
2022-06-05 18:49:57 -0700

    show web vitals

--- src/components/Vitals.jsx
+++ src/components/Vitals.jsx
@@ -1,10 +1,24 @@
 import Card from "./Card";
 import { Table } from "./Collections";
 import Head from "./Head";
+import { getLCP, getFID, getCLS } from "web-vitals";
+import { createResource } from "solid-js";
 
 import styles from "./Vitals.module.scss";
 
+const callbackAsPromise = (fn) => {
+  return async () => {
+    return new Promise((resolve) => {
+      fn(resolve, true);
+    });
+  };
+};
+
 const Vitals = () => {
+  const [LCP] = createResource(callbackAsPromise(getLCP));
+  const [FID] = createResource(callbackAsPromise(getFID));
+  const [CLS] = createResource(callbackAsPromise(getCLS));
+
   return (
     <Card className={styles.Vitals}>
       <Head text="web vitals!" />
@@ -12,7 +26,13 @@ const Vitals = () => {
         Web vitals are a measure of the quality of the user experience of a
         site.
       </p>
-      <Table label="vitals">{[["e", "iou"]]}</Table>
+      <Table label="vitals">
+        {[
+          ["lcp", LCP()?.value ?? "awaiting"],
+          ["fid", FID()?.value ?? "awaiting"],
+          ["cls", CLS()?.value ?? "awaiting"],
+        ]}
+      </Table>
     </Card>
   );
 };
