18898f4767102e6c342a395988a5ba8d455f6a69
Isaac <isaac@rubenfamily.com>
2022-06-05 22:32:32 -0700

    make button a toggle

--- src/components/DisableAnimation.jsx
+++ src/components/DisableAnimation.jsx
@@ -3,24 +3,35 @@ import Head from "./Head";
 import { Code } from "./Copy";
 
 import styles from "./DisableAnimation.module.scss";
+import { createSignal } from "solid-js";
 
-const DisableAnimation = (props) => (
-  <Card>
-    <Head text="overwhelmed?"></Head>
-    <p>
-      Sorry if all the colors are a little garish, or if the motion is
-      overwhelming. It's all gated behind the{" "}
-      <Code>prefers-reduced-motion</Code> query, but you can also turn it off
-      with this big button. Apologies!
-    </p>
-    <button
-      onClick={() => {
-        document.documentElement.classList.add("no_motion");
-      }}
-    >
-      aaaa
-    </button>
-  </Card>
-);
+const DisableAnimation = (props) => {
+  const [status, setStatus] = createSignal(true);
+  return (
+    <Card>
+      <Head text={`${status() ? "over" : "under"}whelmed?`}></Head>
+      <p>
+        Sorry if all the colors are a little garish, or if the motion is
+        overwhelming. It's all gated behind the{" "}
+        <Code>prefers-reduced-motion</Code> query, but you can also turn it{" "}
+        {status() ? "off" : "back on"} with this big button. Apologies!
+      </p>
+      <button
+        className={styles.disable}
+        onClick={() => {
+          console.log(status());
+          if (status()) {
+            document.documentElement.classList.add("no_motion");
+          } else {
+            document.documentElement.classList.remove("no_motion");
+          }
+          setStatus(!status());
+        }}
+      >
+        {status() ? "make it stop!" : "bring it back!"}
+      </button>
+    </Card>
+  );
+};
 
 export default DisableAnimation;
