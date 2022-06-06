cc284f229e3f6fc28602baed04663fbc7b9d9276
Isaac <isaac@rubenfamily.com>
2022-06-05 17:35:43 -0700

    more work on vitals

--- src/components/Vitals.jsx
+++ src/components/Vitals.jsx
@@ -1,8 +1,19 @@
 import Card from "./Card";
+import { Table } from "./Collections";
+import Head from "./Head";
 
 import styles from "./Vitals.module.scss";
 
-const Vitals = (props) => {
-  return <Card>vitals</Card>;
+const Vitals = () => {
+  return (
+    <Card className={styles.Vitals}>
+      <Head text="web vitals!" />
+      <p>
+        Web vitals are a measure of the quality of the user experience of a
+        site.
+      </p>
+      <Table label="vitals">{[["e", "iou"]]}</Table>
+    </Card>
+  );
 };
 export default Vitals;
